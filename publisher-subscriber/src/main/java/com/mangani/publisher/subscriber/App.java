ab837dd1b5a280f842c3e619ac4a6dc6d9f156e3
mangani <singlepattern@gmail.com>
2016-08-03 15:04:21 -0300

    commit

---
+++ publisher-subscriber/src/main/java/com/mangani/publisher/subscriber/App.java
@@ -0,0 +1,13 @@
+package com.mangani.publisher.subscriber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
